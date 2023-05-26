9d97968ec103594d2382f58830075ebd7b933bd3
Augusto Vedana <augusto.vedana@gmail.com>
2023-05-26 13:17:42 -0300

    adicionando cores

---
+++ src/pages/Login/styled.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  background: green;
+
+  small {
+    font-size: 12pt;
+    margin-left: 15px;
+    color: #999;
+  }
+`;
+
+export const Paragrafo = styled.p`
+  font-size: 80px;
+`;
